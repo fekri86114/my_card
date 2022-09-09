08e603c42a63dcdf68724ed2749206099097ab47
fekri1386 <fekri.mohammadreza86@gmail.com>
2022-09-09 14:03:54 +0430

    added fonts and created first design of my-card

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,70 @@ class MyCard extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              CircleAvatar(
+              const CircleAvatar(
                 radius: 50,
                 backgroundColor: Colors.red,
                 backgroundImage: AssetImage('images/my_image.jpg'),
+              ),
+              const Text(
+                'Mohammadreza Fekri',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 38,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'ANDROID DEVELOPER',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: 'SansPro',
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: const EdgeInsets.all(10),
+                child: Row(children: [
+                  const Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    '+98 914 703 1237',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SansPro'),
+                  ),
+                ]),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    const SizedBox(width: 10),
+                    Text('fekri.mohammadreza86@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'SansPro',
+                      fontSize: 20,
+                      color: Colors.teal.shade900
+                    ),)
+                  ],
+                ),
               )
             ],
           ),
